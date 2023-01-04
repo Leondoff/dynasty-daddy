1fc9a972b344e3cc65ed1f4652c73b9e875795a3
AlwaysLuckyBoy <alwaystrustyou@outlook.com>
2023-01-04 16:36:37 -0500

    Bug fix - One match up week support for MFL

--- front-end/fantasy-app/src/app/components/services/wrapped.service.ts
+++ front-end/fantasy-app/src/app/components/services/wrapped.service.ts
@@ -1,54 +1,68 @@
 import { Injectable } from "@angular/core";
+import { LeagueTeam } from "src/app/model/league/LeagueTeam";
 import { LeagueService } from "src/app/services/league.service";
 import { NflService } from "src/app/services/utilities/nfl.service";
 import { TransactionUI } from "../model/transaction";
 import { TransactionsService } from "./transactions.service";
 
 @Injectable({
-    providedIn: 'root'
-  })
-  export class WrappedService {
-    
-    frameNumber: number;
+  providedIn: 'root'
+})
+export class WrappedService {
 
-    /** map of stats to generate and display */
-    transactionsDict = {};
+  frameNumber: number;
 
-    constructor(private leagueService: LeagueService, private transactionsService: TransactionsService, private nflService: NflService) {}
-    
-    /**
-     * Set up transaction dictionary with metrics to show
-     */
-    calculateTransactionStatistics(): void {
-      if (JSON.stringify(this.transactionsService.transactionAggregate) === '{}') {
-        this.transactionsService.generateTransactionAggregate(this.nflService.getCompletedWeekForSeason(this.leagueService.selectedLeague.season));
-      }
-      let totalTrans = [];
-      for (let i = 1; i <= this.leagueService.selectedLeague.totalRosters; i++) {
-        let trans = 0;
-        trans += this.transactionsService.transactionAggregate[i]?.actions || 0;
-        trans += this.transactionsService.transactionAggregate[i]?.trades || 0;
-        totalTrans.push(trans);
-      }
-      this.transactionsDict['total'] = totalTrans.reduce((partialSum, a) => partialSum + a, 0);;
-      this.transactionsDict['max'] = Math.max(...totalTrans);
-      this.transactionsDict['max_id'] = totalTrans.findIndex(e => e === this.transactionsDict['max']) + 1;
-      this.transactionsDict['min'] = Math.min(...totalTrans);
-      this.transactionsDict['min_id'] = totalTrans.findIndex(e => e === this.transactionsDict['min']) + 1;
-      const trades: TransactionUI[] = [];
-      const waivers: TransactionUI[] = [];
-      for (let i = 1; i <= this.leagueService.selectedLeague.totalRosters; i++) {
-        const teamTrans = this.transactionsService.generateTeamTransactionHistory(this.leagueService.getTeamByRosterId(i));
-        teamTrans.forEach(tran => {
-          if(tran.type === 'trade') {
-            trades.push(tran);
-          }
-          if((tran.type === 'waiver' || tran.type === 'free_agent') && tran.adds[0] && tran.adds[0].playerName !== undefined) {
-            waivers.push(tran);
-          }
-        });
-      }
-      this.transactionsDict['trades'] = trades.sort((a,b) => (b.adds.reduce((p, x) => p + x.value, 0) + b.drops.reduce((p, x) => p + x.value, 0)) - (a.adds.reduce((p, x) => p + x.value, 0) + a.drops.reduce((p, x) => p + x.value, 0))).filter((a,i) => i%2===1);
-      this.transactionsDict['waivers'] = waivers.sort((a,b) => Math.abs(b.adds[0].value) - Math.abs(a.adds[0].value));
+  /** map of stats to generate and display */
+  transactionsDict = {};
+
+  constructor(private leagueService: LeagueService, private transactionsService: TransactionsService, private nflService: NflService) { }
+
+  /**
+   * Set up transaction dictionary with metrics to show
+   */
+  calculateTransactionStatistics(): void {
+    if (JSON.stringify(this.transactionsService.transactionAggregate) === '{}') {
+      this.transactionsService.generateTransactionAggregate(this.nflService.getCompletedWeekForSeason(this.leagueService.selectedLeague.season));
+    }
+    let totalTrans = [];
+    for (let i = 1; i <= this.leagueService.selectedLeague.totalRosters; i++) {
+      let trans = 0;
+      trans += this.transactionsService.transactionAggregate[i]?.actions || 0;
+      trans += this.transactionsService.transactionAggregate[i]?.trades || 0;
+      totalTrans.push(trans);
+    }
+    this.transactionsDict['total'] = totalTrans.reduce((partialSum, a) => partialSum + a, 0);;
+    this.transactionsDict['max'] = Math.max(...totalTrans);
+    this.transactionsDict['max_id'] = totalTrans.findIndex(e => e === this.transactionsDict['max']) + 1;
+    this.transactionsDict['min'] = Math.min(...totalTrans);
+    this.transactionsDict['min_id'] = totalTrans.findIndex(e => e === this.transactionsDict['min']) + 1;
+    const trades: TransactionUI[] = [];
+    const waivers: TransactionUI[] = [];
+    for (let i = 1; i <= this.leagueService.selectedLeague.totalRosters; i++) {
+      const teamTrans = this.transactionsService.generateTeamTransactionHistory(this.leagueService.getTeamByRosterId(i));
+      teamTrans.forEach(tran => {
+        if (tran.type === 'trade') {
+          trades.push(tran);
+        }
+        if ((tran.type === 'waiver' || tran.type === 'free_agent') && tran.adds[0] && tran.adds[0].playerName !== undefined) {
+          waivers.push(tran);
+        }
+      });
+    }
+    this.transactionsDict['trades'] = trades.sort((a, b) => (b.adds.reduce((p, x) => p + x.value, 0) + b.drops.reduce((p, x) => p + x.value, 0)) - (a.adds.reduce((p, x) => p + x.value, 0) + a.drops.reduce((p, x) => p + x.value, 0))).filter((a, i) => i % 2 === 1);
+    this.transactionsDict['waivers'] = waivers.sort((a, b) => Math.abs(b.adds[0].value) - Math.abs(a.adds[0].value));
+  }
+
+  /**
+   * sorts a list of teams by the team playoff odds to get winner
+   * @param teams league teams
+   * @param teamPlayoffOdds playoff odds map
+   * @returns 
+   */
+  sortBySeasonWinner(teams: LeagueTeam[], teamPlayoffOdds: {}): LeagueTeam[] {
+    return teams.sort((a, b) => teamPlayoffOdds[b.roster.rosterId].timesWinChampionship - teamPlayoffOdds[a.roster.rosterId].timesWinChampionship ||
+      teamPlayoffOdds[b.roster.rosterId].timesMakeChampionship - teamPlayoffOdds[a.roster.rosterId].timesMakeChampionship ||
+      teamPlayoffOdds[b.roster.rosterId].timesMakeConfRd - teamPlayoffOdds[a.roster.rosterId].timesMakeConfRd
+    );
   }
 }
--- front-end/fantasy-app/src/app/components/wrapped/wrapped-finish/wrapped-finish.component.ts
+++ front-end/fantasy-app/src/app/components/wrapped/wrapped-finish/wrapped-finish.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ConfigService } from 'src/app/services/init/config.service';
 import { LeagueService } from 'src/app/services/league.service';
 import { LeagueSwitchService } from '../../services/league-switch.service';
@@ -8,63 +8,62 @@ import { FadeGrowStagger, FadeSlideInOut } from '../animations/fade.animation';
 import { WrappedCardSummaryContent } from '../wrapped-card-summary/wrapped-card-summary.component';
 
 @Component({
-    selector: 'app-wrapped-finish',
-    templateUrl: './wrapped-finish.component.html',
-    styleUrls: ['./wrapped-finish.component.css'],
-    animations: [FadeSlideInOut, FadeGrowStagger]
+  selector: 'app-wrapped-finish',
+  templateUrl: './wrapped-finish.component.html',
+  styleUrls: ['./wrapped-finish.component.css'],
+  animations: [FadeSlideInOut, FadeGrowStagger]
 })
-  export class WrappedFinishComponent implements OnInit {
+export class WrappedFinishComponent implements OnInit {
 
-    @Input()
-    baseFrame: number;
+  @Input()
+  baseFrame: number;
 
-    /** recap object */
-    summaryCard: WrappedCardSummaryContent
+  /** recap object */
+  summaryCard: WrappedCardSummaryContent
 
-    /** final intro text */
-    finalIntro = [];
-    
-    /** show next button */
-    showNext = false;
-    
-    /** show content */
-    showContent = false;
+  /** final intro text */
+  finalIntro = [];
 
-    constructor(public configService: ConfigService, private leagueService: LeagueService, private playoffCalculatorService: PlayoffCalculatorService, public leagueSwitchService: LeagueSwitchService, public wrappedService: WrappedService) {}
-    
-    ngOnInit(): void {
-      this.finalIntro.push('It\'s tough to say goodbye to football')
-      this.finalIntro.push('But before you go');
-      this.finalIntro.push('We\'ll try to make a recap');
-      const teams = this.leagueService.leagueTeamDetails.slice().sort((a,b) => b.roster.teamMetrics.fpts - a.roster.teamMetrics.fpts);
-      const seasonResults = teams.sort((a,b) => this.playoffCalculatorService.teamPlayoffOdds[b.roster.rosterId].timesWinChampionship - this.playoffCalculatorService.teamPlayoffOdds[a.roster.rosterId].timesWinChampionship ||
-        this.playoffCalculatorService.teamPlayoffOdds[b.roster.rosterId].timesMakeChampionship - this.playoffCalculatorService.teamPlayoffOdds[a.roster.rosterId].timesMakeChampionship);
-      // get best trade
-      const bestTrade = this.wrappedService.transactionsDict['trades']?.[0] || null;
-      const tradeTeam1 = bestTrade ? this.leagueService.getTeamByRosterId(bestTrade.rosterIds[0]) : null;
-      const tradeTeam2 = bestTrade ? this.leagueService.getTeamByRosterId(bestTrade.rosterIds[1]) : null;
-      const biggestTrade = bestTrade ? {team1: tradeTeam1.owner.teamName, team2: tradeTeam2.owner.teamName, team1Adds:bestTrade.adds.map(p => p.playerName), team2Adds: bestTrade.drops.map(p => p.playerName)} : null;
-      this.summaryCard = {first: seasonResults[0], second: seasonResults[1], bestOffense: teams[0], worstOffense: teams[teams.length-1], bestTrade: biggestTrade, standings: this.leagueService.leagueTeamDetails.slice(), totalWaivers: this.wrappedService.transactionsDict['waivers']?.length || 0, totalTrades: this.wrappedService.transactionsDict['trades']?.length || 0, leagueName: this.leagueService.selectedLeague.name};
-      setInterval(()=> {
-        this.showContent = true;
-      } ,1000);
-      setInterval(()=> {
-        this.showNext = true;
-      } ,3000);
-    }
+  /** show next button */
+  showNext = false;
 
-    /**
-    * handles transitioning to the next frame
-    */
-    nextFrame(): void {
-      this.wrappedService.frameNumber++;
-      this.showNext = false;
-      this.showContent = false;
-      setInterval(()=> {
-        this.showContent = true;
-      } ,1000);
-      setInterval(()=> {
-        this.showNext = true;
-      } ,12000);
-    }
+  /** show content */
+  showContent = false;
+
+  constructor(public configService: ConfigService, private leagueService: LeagueService, private playoffCalculatorService: PlayoffCalculatorService, public leagueSwitchService: LeagueSwitchService, public wrappedService: WrappedService) { }
+
+  ngOnInit(): void {
+    this.finalIntro.push('It\'s tough to say goodbye to football')
+    this.finalIntro.push('But before you go');
+    this.finalIntro.push('We\'ll try to make a recap');
+    const teams = this.leagueService.leagueTeamDetails.slice().sort((a, b) => b.roster.teamMetrics.fpts - a.roster.teamMetrics.fpts);
+    const seasonResults = this.wrappedService.sortBySeasonWinner(teams, this.playoffCalculatorService.teamPlayoffOdds);
+    // get best trade
+    const bestTrade = this.wrappedService.transactionsDict['trades']?.[0] || null;
+    const tradeTeam1 = bestTrade ? this.leagueService.getTeamByRosterId(bestTrade.rosterIds[0]) : null;
+    const tradeTeam2 = bestTrade ? this.leagueService.getTeamByRosterId(bestTrade.rosterIds[1]) : null;
+    const biggestTrade = bestTrade ? { team1: tradeTeam1.owner.teamName, team2: tradeTeam2.owner.teamName, team1Adds: bestTrade.adds.map(p => p.playerName), team2Adds: bestTrade.drops.map(p => p.playerName) } : null;
+    this.summaryCard = { first: seasonResults[0], second: seasonResults[1], bestOffense: teams[0], worstOffense: teams[teams.length - 1], bestTrade: biggestTrade, standings: this.leagueService.leagueTeamDetails.slice(), totalWaivers: this.wrappedService.transactionsDict['waivers']?.length || 0, totalTrades: this.wrappedService.transactionsDict['trades']?.length || 0, leagueName: this.leagueService.selectedLeague.name };
+    setInterval(() => {
+      this.showContent = true;
+    }, 1000);
+    setInterval(() => {
+      this.showNext = true;
+    }, 3000);
+  }
+
+  /**
+  * handles transitioning to the next frame
+  */
+  nextFrame(): void {
+    this.wrappedService.frameNumber++;
+    this.showNext = false;
+    this.showContent = false;
+    setInterval(() => {
+      this.showContent = true;
+    }, 1000);
+    setInterval(() => {
+      this.showNext = true;
+    }, 12000);
+  }
 }
