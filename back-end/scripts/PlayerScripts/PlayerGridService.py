4a52b0bcc8c90f7b798ae9ff8af5655c8bee8071
AlwaysLuckyBoy <alwaystrustyou@outlook.com>
2023-07-29 10:43:27 -0400

    [gridiron] reduce odds of colleges

--- back-end/scripts/PlayerScripts/PlayerGridService.py
+++ back-end/scripts/PlayerScripts/PlayerGridService.py
@@ -5,7 +5,7 @@ import json
 import os
 import psycopg2
 
-SupportedYTypes = ['college', 'team', 'team', 'stat', 'stat']
+SupportedYTypes = ['college', 'team', 'team', 'team', 'stat', 'stat', 'stat']
 
 SupportedXTypes = ['award', 'stat', 'stat']
 
