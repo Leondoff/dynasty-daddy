d488023f6c4765a3a47da94d4c7b50bbedee78bb
AlwaysLuckyBoy <alwaystrustyou@outlook.com>
2023-10-04 14:14:54 -0400

    [actions] add secrets inherit

--- back-end/express-api/src/controllers/userController.js
+++ back-end/express-api/src/controllers/userController.js
@@ -8,7 +8,6 @@ export const GetTokenForPatreonCodeEndpoint = async (req, res) => {
   const { code } = req.query;
 
   try {
-    console.log('CLIENT - ', PATREON_CLIENT_ID);
     // Step 1: Get the access token
     const tokenResponse = await axios.post(
       'https://www.patreon.com/api/oauth2/token',
