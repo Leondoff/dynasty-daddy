630e8b3540021bf16d7216f941a37f2a189fa9a3
AlwaysLuckyBoy <alwaystrustyou@outlook.com>
2023-10-26 12:50:05 -0400

    [sentry] added sentry for improving telemetry

--- front-end/fantasy-app/src/main.ts
+++ front-end/fantasy-app/src/main.ts
@@ -1,12 +1,32 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments';
+import * as Sentry from "@sentry/angular-ivy";
+
+Sentry.init({
+  dsn: "https://1d57bd69aaa9e7a91244a2764df6311a@o4506117484904448.ingest.sentry.io/4506117486477312",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/dynasty-daddy\.com/ ],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((success) => console.log('Bootstrap success'))
+  .catch((err) => console.error(err));
