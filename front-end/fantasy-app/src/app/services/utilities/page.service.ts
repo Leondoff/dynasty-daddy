8ac29cc5fbb757e6cdf14f88580cab886e961216
AlwaysLuckyBoy <alwaystrustyou@outlook.com>
2023-11-22 14:06:18 -0500

    [league format] fixed bugs with league format

--- front-end/fantasy-app/src/app/services/utilities/page.service.ts
+++ front-end/fantasy-app/src/app/services/utilities/page.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
+import { Injectable, Inject } from "@angular/core";
 import { Meta, Title } from "@angular/platform-browser";
 
 @Injectable({
@@ -13,13 +14,16 @@ export class PageService {
     GRIDIRON_IMG = 'https://i.ibb.co/mNk4j0H/SEO-Image-1.jpg';
 
     defaultKeywords: string[] = ['fantasy', 'player', 'sleeper', 'rankings', 'analyzer', 'league', 'trade calulator', 'dynasty', 'redraft', 'keeper',
-     'football', 'player rankings', 'free fantasy tool', 'trade analyzer', 'league analyzer', 'league power rankings', 'fantasy simulator'];
+        'football', 'player rankings', 'free fantasy tool', 'trade analyzer', 'league analyzer', 'league power rankings', 'fantasy simulator'];
 
     constructor(
         private meta: Meta,
-        private title: Title) {}
+        @Inject(DOCUMENT) private doc,
+        private title: Title
+    ) { }
 
-    setUpPageSEO(title: string, keywords: string[], description: string): void { 
+    setUpPageSEO(title: string, keywords: string[], description: string): void {
+        this.createLinkForCanonicalURL();
         if (title == 'Home') {
             this.title.setTitle('Dynasty Daddy - Fantasy Football Tools, and Rankings');
         } else {
@@ -35,27 +39,50 @@ export class PageService {
         if (this.meta.getTag('keywords')) {
             this.meta.updateTag({ name: 'keywords', content: keywordList.join(', ') });
             this.meta.updateTag({ name: 'description', content: description });
-            this.meta.updateTag({ name: 'og:description', content: description});
-            this.meta.updateTag({ name: 'twitter:description', content: description});
-            this.meta.updateTag({ name: 'og:title', content: title});
-            this.meta.updateTag({ name: 'twitter:title', content: title});
-            this.meta.updateTag({ name: 'og:url', content: 'https://dynasty-daddy.com/' + url});
-            this.meta.updateTag({ name: 'twitter:description', content: 'https://dynasty-daddy.com/' + url});
+            this.meta.updateTag({ name: 'og:description', content: description });
+            this.meta.updateTag({ name: 'twitter:description', content: description });
+            this.meta.updateTag({ name: 'og:title', content: title });
+            this.meta.updateTag({ name: 'twitter:title', content: title });
+            this.meta.updateTag({ name: 'og:url', content: 'https://dynasty-daddy.com/' + url });
+            this.meta.updateTag({ name: 'twitter:description', content: 'https://dynasty-daddy.com/' + url });
         } else {
             this.meta.addTags([
                 { name: 'keywords', content: keywordList.join(', ') },
                 { name: 'description', content: description },
-                { name: 'og:title', content: title},
-                { name: 'og:description', content: description},
-                { name: 'og:url', content: 'https://dynasty-daddy.com/' + url},
-                { name: 'og:image', content: img},
-                { name: 'og:site_name', content: 'Dynasty Daddy'},
-                { name: 'twitter:card', content: 'summary_large_image'},
-                { name: 'twitter:title', content: title},
-                { name: 'twitter:description', content: description},
-                { name: 'twitter:url', content: 'https://dynasty-daddy.com/' + url},
-                { name: 'twitter:image', content: img},
-            ]);    
+                { name: 'og:title', content: title },
+                { name: 'og:description', content: description },
+                { name: 'og:url', content: 'https://dynasty-daddy.com/' + url },
+                { name: 'og:image', content: img },
+                { name: 'og:site_name', content: 'Dynasty Daddy' },
+                { name: 'twitter:card', content: 'summary_large_image' },
+                { name: 'twitter:title', content: title },
+                { name: 'twitter:description', content: description },
+                { name: 'twitter:url', content: 'https://dynasty-daddy.com/' + url },
+                { name: 'twitter:image', content: img },
+            ]);
         }
     }
+
+    /**
+     * create canonical tags for html
+     */
+    private createLinkForCanonicalURL() {
+        // Check if the canonical link already exists
+        let existingLink = this.doc.querySelector('link[rel="canonical"]');
+      
+        // Strip URL parameters
+        let canonicalURL = this.doc.URL.split('?')[0];
+      
+        if (existingLink) {
+          // If it exists, update the href attribute
+          existingLink.setAttribute('href', canonicalURL);
+        } else {
+          // If it doesn't exist, create a new link element
+          let link = this.doc.createElement('link');
+          link.setAttribute('rel', 'canonical');
+          link.setAttribute('href', canonicalURL);
+          this.doc.head.appendChild(link);
+        }
+      }      
+
 }
